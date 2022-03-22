ebe757b86d6edad54f76bff0a984edccbb253429
rocean <rocean74@gmail.com>
2022-03-22 04:50:50 +0200

    Remove import

--- src/main/java/eu/apps4net/pocketparser/PocketParserApplication.java
+++ src/main/java/eu/apps4net/pocketparser/PocketParserApplication.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
